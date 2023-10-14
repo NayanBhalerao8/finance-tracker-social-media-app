cf8f5f722ab4b2b542d3de71b0ef7813c4b49bfc
Nayan Bhalerao <rubydev.nayan@gmail.com>
2023-10-14 15:49:57 +0530

    adding bootstrap4 errors and resolve babale js error and import jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,11 @@ import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
+import "bootstrap";
+import "jquery";
+
+// global.$ = require("jquery")
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
-
-import "bootstrap";
